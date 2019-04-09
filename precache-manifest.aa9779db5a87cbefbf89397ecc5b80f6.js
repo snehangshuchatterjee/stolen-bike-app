0a875d5afda485ff78919a578f185aea55fabee6
Snehangshu Chatterjee <snehangshu.chatterjee@pearson.com>
2019-04-10 00:14:39 +0530

    Updates

---
+++ precache-manifest.aa9779db5a87cbefbf89397ecc5b80f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7148954da7d64aefe90c73fcee0e30bc",
+    "url": "/stolen-bike-app/static/media/Berlin_police_logo.7148954d.png"
+  },
+  {
+    "revision": "596cc6617c7ed407593a",
+    "url": "/stolen-bike-app/static/js/runtime~main.0a0afeba.js"
+  },
+  {
+    "revision": "ce30afb37909900a5529",
+    "url": "/stolen-bike-app/static/js/main.0a4595d6.chunk.js"
+  },
+  {
+    "revision": "b09a8b547c8fb92af349",
+    "url": "/stolen-bike-app/static/js/2.b7c46284.chunk.js"
+  },
+  {
+    "revision": "ce30afb37909900a5529",
+    "url": "/stolen-bike-app/static/css/main.9c040e9e.chunk.css"
+  },
+  {
+    "revision": "6e0e7fbff7a77ddaa5d669557c0c10b4",
+    "url": "/stolen-bike-app/index.html"
+  }
+];
